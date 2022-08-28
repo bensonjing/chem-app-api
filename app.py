525509f862b29aabdc4977d6d1ce34631873b649
Benson Jing <bensonjing@icloud.com>
2022-08-28 01:43:55 -0400

    implement index route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 
 import result
 
@@ -7,6 +7,12 @@ app = Flask(__name__)
 
 
 count = 0
+
+
+@app.route('/')
+def index_get():
+    return 'hello'
+    # return redirect(url_for('pic'))
 
 
 @app.post('/pic')
@@ -24,5 +30,5 @@ def pic_post():
     return {"result": resultInt}
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
